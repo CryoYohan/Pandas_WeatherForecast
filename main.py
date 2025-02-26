ddd77ef06b0f42e5a1f5a2e6547ad8a38d858c68
itsmecyril <cyrilypil@gmail.com>
2025-02-26 22:33:51 +0800

    added dataset for squirrel census data in central park new york in 2018

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Weather Forecast
 ### Whereas pandas consist only three lines, pandas are more efficient in data analysis and reading csv
 
 import pandas as pd
-data = pd.read_csv('weather_data.csv')
+# data = pd.read_csv('weather_data.csv')
 
 # Get Column Temperature which is a Data Series object
-temp_list = data['temp'].to_list()
+# temp_list = data['temp'].to_list()
 
 # Get the Average Temperature the Traditional way and using Pandas, see the comparison:
 #print(f"Average Temperature: {round(sum(temp_list)/len(temp_list),2)}")
@@ -23,22 +23,24 @@ temp_list = data['temp'].to_list()
 # print(data[data.temp == data.temp.max()])
 
 # Get Row Data for One Single Day
-monday = data[data.day=='Monday']
-print(monday)
+# monday = data[data.day=='Monday']
+# print(monday)
 
 # Get monday's temperature that is in Celsius then convert it into Fahrenheit
-monday_temperature = monday.temp[0]
-print(f"Monday's Temperature in Celsius: {monday_temperature} Degrees Celsius\nMonday's Temperature in Fahrenheit: {((9/5)*monday_temperature)+32} Degrees Fahrenheit")
+# monday_temperature = monday.temp[0]
+# print(f"Monday's Temperature in Celsius: {monday_temperature} Degrees Celsius\nMonday's Temperature in Fahrenheit: {((9/5)*monday_temperature)+32} Degrees Fahrenheit")
 
 # Create a dataframe from scratch
-student_data = {
-    'students': ['Anna', 'John', 'Ezekiel', 'Joshua'],
-    'scores': [96,97,88,86],
-}
-new_data = pd.DataFrame(student_data)
-print(new_data)
+# student_data = {
+#     'students': ['Anna', 'John', 'Ezekiel', 'Joshua'],
+#     'scores': [96,97,88,86],
+# }
+# new_data = pd.DataFrame(student_data)
+# print(new_data)
 
 # Convert new data frame to csv
 # new_data.to_csv('new_data.csv')
+
+
 
 
